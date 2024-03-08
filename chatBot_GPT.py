99c3ae454ddcba1f6c81ffa8c042aba54e1dd628
hongyaong <ng_hong_yao@hotmail.com>
2024-03-08 15:18:38 +0800

    merge with jy

--- chatBot_GPT.py
+++ chatBot_GPT.py
@@ -1,13 +1,17 @@
 import os
+from dotenv import load_dotenv
 import prompt_template
 from prompt_template import template_2
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 from langchain import OpenAI, ConversationChain, LLMChain, PromptTemplate
-from langchain.chains import ConversationChain
+from langchain.chains import ConversationChain, ConversationSummaryBufferMemory
 from langchain.memory import ConversationBufferMemory
+from langchain_openai import OpenAIEmbeddings
 from pymongo import MongoClient
-
+import requests
+from bs4 import BeautifulSoup
+from newspaper import Article
 import datetime
 #import blocks from our blocks python file
 import blocks
@@ -23,6 +27,7 @@ from slack_sdk.errors import SlackApiError
 #--------------------------------------------------------------------------------------------------------------------
 # Initializes your app with your bot token and socket mode handler
 # If using local environ, replace with app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
+load_dotenv()
 app = App(token=os.getenv("SLACK_BOT_TOKEN"), ignoring_self_events_enabled = False) 
 api_key = os.getenv("OPENAI_API_KEY")
 template_2 = template_2
@@ -34,35 +39,149 @@ collection = db.get_collection("collection")
 
 
 #Langchain implementation
-template = """ You are speaking to a professional who does not have much time, do an informative summary in 7 sentences maximum and keep the answer concise.
-    If you do not know the title or link or date, just state TBC
-    Go through the information and make a summary of the information in this following format with their answers: 
+template1 = """ You are a bot that will either provide news recommendations, news summaries or answer questions related to the news asked by users.
+    You are speaking to a professional so keep the answer informative and concise.
+        
+    If asked for news recommendations based on certain keywords or topics, return the recommendations using the additional information provided in this format:
+    The ordering of the article recommendation should be based on the order of your previous output and not the order given by the additional information.
 
     Title: <Title Name>
     Website Link: <Link of Website>
-    Date of Article: <Get the latest date of publication, regardless of update or publish>
+    Date of Article: <Get the latest date of publication>
+
+    If not, just answer the questions with the information you know.
+
+
+        Additional Information: {add_info}
+
+        History:{history}
+
+        Human: {human_input}
+        Assistant:"""
+
+template2 = """ You are a bot that will either provide news recommendations, news summaries or answer questions related to the news asked by users.
+    You are speaking to a professional so keep the answer informative and concise.
+
+    You are given an article(s) to summarize. Please respond with the following using information given.
+    Title: <Title Name>
+    Website Link: <Link of Website>
+    Date of Article: <Get the latest date of publication>
     Names to note: <Names of Company and people mentioned within the article>
     Key Topic: <Key topic of this article>
     Sentiment: <conduct sentiment analysis and let them know the sentiment>
     Trends & Statistics: <Include any trends and statistics found, make it short and do not repeat it in summary>
-
     Summary: <The summarised version of the article>
-    
-    {history}
-    Human: {human_input}
-    Assistant:"""
 
-prompt = PromptTemplate(
-    input_variables=["history", "human_input"], 
-    template=template
+        Additional Information: {add_info}
+
+        Human: {human_input}
+        Assistant:
+"""
+prompt1 = PromptTemplate(
+    input_variables=["history", "human_input", "add_info"], 
+    template=template1
 )
 
-chatgpt_chain = LLMChain(
-    llm = OpenAI(model="gpt-3.5-turbo-instruct", temperature=0), 
-    prompt=prompt, 
+prompt2 = PromptTemplate(
+    input_variables=["history", "human_input", "add_info"], 
+    template=template2
+)
+
+chatgpt_chain1 = LLMChain(
+    llm = OpenAI(openai_api_key=api_key,model="gpt-3.5-turbo-instruct", temperature=0), 
+    prompt=prompt1, 
     verbose=True, 
-    memory=ConversationBufferMemory(k=2),
+    memory=ConversationBufferMemory(memory_key="history", input_key="human_input")
 )
+
+chatgpt_chain2 = LLMChain(
+    llm = OpenAI(openai_api_key=api_key,model="gpt-3.5-turbo-instruct", temperature=0), 
+    prompt=prompt2, 
+    verbose=True, 
+    memory=ConversationBufferMemory(memory_key="history", input_key="human_input")
+)
+
+
+article_embeddings = OpenAIEmbeddings(model="text-embedding-3-large", dimensions=1536) # model used to embed article
+query_embeddings = OpenAIEmbeddings(model="text-embedding-3-small", dimensions=1536) # model used to embed user queries
+
+def vector_search(query, collection):
+    query_embedding = query_embeddings.embed_query(query)
+    pipeline = [
+        {
+            "$vectorSearch": { # $vectorSearch is the specific function name
+                "index": "vector_index", # The search index I created on MongoDB
+                "queryVector": query_embedding, # The embedded query from the user that is used for searching
+                "path": "data", # The relevant field of the document that is used for searching (in this case the full text of the news article)
+                "limit": 5, # How many results you want the vectorSearch to show
+                "numCandidates": 100 # How many documents you want vectorSearch to consider when searching
+            }
+        }
+    ]
+    
+    results = collection.aggregate(pipeline) # executing the search
+    return list(results) # compile results into a list
+
+
+def scrape_and_store(url):
+    url = url[1:-1]
+    article = Article(url)
+    article.download()
+    article.parse()
+    title = article.title
+    content = article.text
+    embeddings = article_embeddings.embed_query(content)
+    article_json = {
+        'title': title,
+        'link': url,
+        'data': embeddings,
+        'article': content,
+        # date how ah?        
+    }
+
+    collection.insert_one(article_json)
+   
+    output = {
+        "Title": title,
+        "Link": url,
+        "Article": content
+    }
+
+    return output
+
+
+def find_url(text):
+    x = text.split()
+    url = []
+    for word in x:
+        if word.startswith("<https:") or word.startswith("<http:") or word.startswith("<www."):
+            url.append(word)
+    return url
+
+def user_query(query):
+    url = find_url(query)
+    print(url)
+    
+    if url: # if array not empty
+        output = ''
+        articles = []
+        for x in url:
+            try:
+                article = scrape_and_store(x)
+                articles.append(article)
+                output = chatgpt_chain2.predict(human_input = query, add_info=articles)
+            except:
+                output = "There was a problem accessing one of the links. Please ensure that the link is working."
+                break
+        return output
+    else:
+        get_knowledge = vector_search(query, collection) # Get the output from MongoDB after vector searching
+
+        search_result = ''
+        for result in get_knowledge:
+            search_result += f"Title: {result.get('title', 'N/A')}, Link: {result.get('link', 'N/A')}"
+        output = chatgpt_chain1.predict(human_input = query, add_info = search_result)  
+        return output
 
 #--------------------------------------------------------------------------------------------------------------------
 #               SCHEDULE MESSAGE Slack API 
@@ -244,9 +363,9 @@ def messaage_handler(message, say, logger):
         say(hy_readDbFunctions.getLatestNewsCategorized(chatgpt_chain, collection, cleaned_selected_categories))
     
     elif message['channel_type'] != 'channel' and 'bot_id' not in message.keys():
-        # output = chatgpt_chain.predict(human_input = message['text'])   
-        # say(output)
-        say("not connected to chatgpt, testing only")
+        response = user_query(message['text'])
+
+        say(response)
 
 #--------------------------------------------------------------------------------------------------------------------
 #               Slackbot startup
